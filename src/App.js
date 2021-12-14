fbad0cb85894e4b4b8069e99ae72569e60af5a57
alessamohammed <mohammed@alessamohammed.com>
2021-12-14 23:10:30 +0300

    adding course component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './Components/navbar';
+import Course from './Components/course';
 function App() {
   return (
     <div className="">
   
        
   <Navigation/>
-         
+  <Course
+  title={"Artificial Intelligence Masterclass"} 
+  image={"https://cdn.pixabay.com/photo/2020/07/31/06/56/ai-5452104_960_720.jpg"}
+  description={"Enter the new era of hybrid ai models optimized by deep neuroevolution, with a complete toolkit of ml, dl & ai models"}
+  provider={"Udemy"}
+  cost={"Free"}
+  />
     </div>
   );
 }
